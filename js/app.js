16d8b9df9eb02d87ed448877f3fe48fdb5d5540f
Katia Yoshie Sato <katia.eumesma@gmail.com>
2026-09-16 16:12:24 -0300

    Add second project gallery and blood donation project

--- js/app.js
+++ js/app.js
@@ -1,23 +1,25 @@
-const mainImage = document.querySelector("#project-main-image");
-const thumbnails = document.querySelectorAll(".project-thumbnail");
+const galleries = document.querySelectorAll(".project-gallery");
 
-thumbnails.forEach((thumbnail) => {
-  thumbnail.addEventListener("click", () => {
+galleries.forEach((gallery) => {
+  const mainImage = gallery.querySelector(".project-main-image");
+  const thumbnails = gallery.querySelectorAll(".project-thumbnail");
 
-    mainImage.classList.add("changing");
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", () => {
+      mainImage.classList.add("changing");
 
-    setTimeout(() => {
-      const mainSrc = mainImage.src;
-      const mainAlt = mainImage.alt;
+      setTimeout(() => {
+        const mainSrc = mainImage.src;
+        const mainAlt = mainImage.alt;
 
-      mainImage.src = thumbnail.src;
-      mainImage.alt = thumbnail.alt;
+        mainImage.src = thumbnail.src;
+        mainImage.alt = thumbnail.alt;
 
-      thumbnail.src = mainSrc;
-      thumbnail.alt = mainAlt;
+        thumbnail.src = mainSrc;
+        thumbnail.alt = mainAlt;
 
-      mainImage.classList.remove("changing");
-    }, 180);
-
+        mainImage.classList.remove("changing");
+      }, 180);
+    });
   });
 });
